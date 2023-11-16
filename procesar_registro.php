4c2d4dbe1cdc4401dd3138d42e02ca8b4086e396
fercho672 <105996745+fercho672@users.noreply.github.com>
2023-11-16 03:47:07 -0500

    Update procesar_registro.php

--- procesar_registro.php
+++ procesar_registro.php
@@ -1,26 +1,71 @@
 <?php
+// ... tu código existente ...
+
+// Verifica si se ha enviado el formulario
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Recupera los datos del formulario
+    // Recibe los datos del formulario
     $nombre = $_POST["nombre"];
     $apellido = $_POST["apellido"];
     $email = $_POST["correo"];
-    $contraseña = $_POST["contrasena"];
+    $contrasena = $_POST["contrasena"];
 
-    // Realiza el procesamiento necesario, como almacenar en una base de datos
+    // Genera un código de confirmación
+    $codigoConfirmacion = generarCodigoAleatorio();
 
-    // Envia el correo de confirmación
-    $to = $email;
-    $subject = "Confirmación de Registro";
-    $message = "Gracias por registrarte, $nombre $apellido. Tu registro ha sido confirmado.";
-    $headers = "From: tu@email.com";
+    // Inserta los datos en la base de datos (ajusta esto según tu estructura de BD)
+    $stmt = $conn->prepare("INSERT INTO usuarios (nombre, apellido, correo, contrasena, codigo_confirmacion) VALUES (?, ?, ?, ?, ?)");
+    $stmt->bind_param("sssss", $nombre, $apellido, $email, $contrasena, $codigoConfirmacion);
 
-    mail($to, $subject, $message, $headers);
+    if ($stmt->execute()) {
+        // Envía el correo de confirmación
+        $para = $email;
+        $asunto = "Confirmación de Registro en Paid Taxi";
+        $mensaje = "¡Gracias por registrarte en Paid Taxi! Tu código de confirmación es: $codigoConfirmacion";
 
-    // Redirige a la página3.html
-    header("Location: pagina3.html");
-    exit();
+        // Ajusta estos valores según tu configuración de Email.js
+        $usuario_emailjs = "user_ayJFj7Wg1PyKi7YUN2fJR";
+        $template_emailjs = "template_1ivqvxn";
+        $servicio_emailjs = "service_9de3y3i";
+
+        // Envía el correo a través de Email.js
+        $emailjs_url = "https://api.emailjs.com/api/v1.0/email/send";
+        $data = array(
+            'service_id' => $servicio_emailjs,
+            'template_id' => $template_emailjs,
+            'user_id' => $usuario_emailjs,
+            'template_params' => json_encode(array('to_email' => $para, 'subject' => $asunto, 'message' => $mensaje))
+        );
+
+        $options = array(
+            'http' => array(
+                'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+                'method' => 'POST',
+                'content' => http_build_query($data),
+            ),
+        );
+
+        $context = stream_context_create($options);
+        $result = file_get_contents($emailjs_url, false, $context);
+
+        if ($result !== FALSE) {
+            // Redirige a la página de confirmación
+            header("Location: pagina3.html");
+            exit;
+        } else {
+            // Manejo de errores al enviar el correo
+            echo "Error al enviar el correo de confirmación.";
+        }
+    } else {
+        // Manejo de errores al insertar en la base de datos
+        echo "Error al registrar el usuario.";
+    }
+
+    $stmt->close();
 }
+
+$conn->close();
 ?>
+
 
 
 
