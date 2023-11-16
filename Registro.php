19ee295f342ae9e24add66a305a2b64e4aa89a4c
fercho672 <105996745+fercho672@users.noreply.github.com>
2023-11-16 03:59:53 -0500

    Update Registro.php

--- Registro.php
+++ Registro.php
@@ -1,19 +1,33 @@
-<?php
-// ... (código de conexión)
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <!-- ... tus encabezados existentes ... -->
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <!-- Agrega jQuery para facilitar las solicitudes AJAX -->
+    <script src="https://cdn.emailjs.com/dist/email.min.js"></script>
+    <!-- Agrega Email.js para enviar correos electrónicos -->
+    <script>
+        emailjs.init("ayJFj7Wg1PyKi7YUN2fJR"); // Reemplaza "tu_user_id" con tu ID de usuario de Email.js
 
-$nombre = $_POST['nombre'];
-$apellido = $_POST['apellido'];
-$correo = $_POST['correo'];
-$contrasena = password_hash($_POST['contrasena'], PASSWORD_DEFAULT);
+        function enviarCorreoConfirmacion(emailDestino) {
+            // Configura el contenido del correo
+            var contenidoCorreo = {
+                to_email: emailDestino,
+                subject: "Confirmación de Registro",
+                message: "¡Gracias por registrarte en Paid Taxi! Tu registro ha sido exitoso.",
+            };
 
-$sql = "INSERT INTO usuarios (nombre, apellido, correo, contrasena) VALUES ('$nombre', '$apellido', '$correo', '$contrasena')";
-
-if ($conexion->query($sql) === TRUE) {
-    echo "Registro exitoso";
-} else {
-    echo "Error: " . $sql . "<br>" . $conexion->error;
-}
-
-// Cerrar la conexión
-$conexion->close();
-?>
+            // Envía el correo
+            emailjs.send("service_9de3y3i", "template_1ivqvxn", contenidoCorreo)
+                .then(function(response) {
+                    console.log("Correo enviado con éxito", response);
+                })
+                .catch(function(error) {
+                    console.log("Error al enviar el correo", error);
+                });
+        }
+    </script>
+    <title>Paid Taxi</title>
+</head>
+<!-- ... resto de tu HTML ... -->
+</html>
