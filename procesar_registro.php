c5ac49f785f9e057be6aeb2abc21c90c109de340
fercho672 <105996745+fercho672@users.noreply.github.com>
2023-11-16 03:01:40 -0500

    Update procesar_registro.php

--- procesar_registro.php
+++ procesar_registro.php
@@ -1,15 +1,23 @@
 <?php
-$servidor = "localhost";
-$usuario = "root";
-$contrasena = "";
-$base_datos = "paid_taxi_db";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombre = $_POST["nombre"];
+    $apellido = $_POST["apellido"];
+    $correo = $_POST["correo"];
+    $contrasena = $_POST["contrasena"];
+    $confirmarContrasena = $_POST["confirmar_contrasena"];
 
-// Crear la conexión
-$conexion = new mysqli($servidor, $usuario, $contrasena, $base_datos);
+    // Validar datos aquí...
 
-// Verificar la conexión
-if ($conexion->connect_error) {
-    die("Conexión fallida: " . $conexion->connect_error);
+    // Enviar correo de confirmación (necesitarás configurar un servidor de correo o usar un servicio de envío de correo)
+    $asunto = "Confirmación de Registro en Paid Taxi";
+    $mensaje = "¡Gracias por registrarte en Paid Taxi, $nombre $apellido! Tu registro ha sido confirmado.";
+
+    // Ajusta la siguiente línea para configurar el remitente y el encabezado del correo
+    mail($correo, $asunto, $mensaje, 'From: tu@email.com');
+
+    // Puedes redirigir a una página de confirmación después de enviar el correo
+    header("Location: confirmacion_registro.html");
+    exit;
 }
 ?>
 
